111f06d53fa8e465b697a1a3e2fadec033b4ad2c
Ray <rafaelgame2012@gmail.com>
2021-07-06 21:27:08 -0500

    urls fix

--- Backend/Backend/conf/urls/products.py
+++ Backend/Backend/conf/urls/products.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+# urlpatterns = [
     
-]
+# ]
+from rest_framework.routers import DefaultRouter
+from apiproducts.views import ProductViewSet
+
+router = DefaultRouter()
+router.register('api/products', ProductViewSet)
+
+urlpatterns = router.urls
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -2,16 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from Backend.conf.urls import products, users
-from rest_framework.routers import DefaultRouter
-from apiproducts.views import ProductViewSet
-
-router = DefaultRouter()
-router.register('products', ProductViewSet)
-
-urlpatterns_api = router.urls
 
 urlpatterns_init = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns = urlpatterns_init + urlpatterns_api + products.urlpatterns + users.urlpatterns
+urlpatterns = urlpatterns_init + products.urlpatterns + users.urlpatterns
