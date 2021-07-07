a3a3e8efb945412a72f496e07b8ba6bd1f72e507
Samuel <samuelalva02@gmail.com>
2021-07-06 22:43:27 -0500

    config

---
+++ src/Pages/Maps.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Maps extends Component{
+    render(){
+        return(
+            <>
+            <h2>Mapa</h2>
+            </>
+        )
+    }
+}
---
+++ src/Pages/Store.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Store extends Component{
+    render(){
+        return(
+            <>
+            <h2>Tienda</h2>
+            </>
+        )
+    }
+}
