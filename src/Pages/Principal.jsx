17f2623fa22abc765244bdad8aac831c29469656
Fernando Maldonado <ferjml97@gmail.com>
2021-07-07 07:00:25 -0500

    updateLanding

--- src/Pages/Principal.jsx
+++ src/Pages/Principal.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import banner from '../img/Banner.png';
+import bannerDos from '../img/Banner2.png';
 
 import Principal from './Principal.css'
 const Contador = () => {
@@ -9,13 +10,20 @@ const Contador = () => {
     return (
         <Fragment>
           
-            <div class="d-flex justify-content-center mt-5 ">
-                <div>
-                    <img src={banner} alt='Obio'/>
+            <div style={{ height: 600 }}>
+                <div class="hero-image">
+                    <img src="" alt=''/>
+                    <div class="ms-2 mt-3 p-5">
+                        <h1>ES OBIO Y ES JUSTO</h1>
+                        <h1>ES OBIO RESPETAR EL ENTORNO</h1>
+                        <h1>ES OBIO SER NATURAL</h1>
+                        <h1>ES OBIO Y ES ORGÁNICO</h1>
+                    </div>
                 </div>
+                
             </div>
 
-            <div class="d-flex justify-content-center p-3 mt-4">
+            <div class="d-flex justify-content p-5 ms-2 mt-4">
                 <div>  {/*  1   */}
                     <div class="content-center">
                         <img src="https://sc04.alicdn.com/kf/Hfe6f8e4615ae4f0e88817e8656da500fX.jpg" alt="café"></img>
@@ -48,17 +56,35 @@ const Contador = () => {
                         <button>Agregar al carrito</button>
                     </div>
                 </div>
-            </div>
 
-            {/*<div class="d-flex justify-content bg-warning">*/}
-            
-            
-                <div class="container">
-                    <img src="" alt=""/>
-                    <div class=" mt-3">
-
+                <div>  {/*  4   */}
+                    <div class="content-center">
+                        <img src="https://sc04.alicdn.com/kf/Hfe6f8e4615ae4f0e88817e8656da500fX.jpg" alt="café"></img>
+                    </div>
+                    <div>
+                        <h3>Café</h3>
+                        <h4>$140</h4>
+                        <button>Agregar al carrito</button>
                     </div>
                 </div>
+
+                <div>  {/*  5   */}
+                    <div class="content-center">
+                        <img src="https://sc04.alicdn.com/kf/Hfe6f8e4615ae4f0e88817e8656da500fX.jpg" alt="café"></img>
+                    </div>
+                    <div>
+                        <h3>Café</h3>
+                        <h4>$140</h4>
+                        <button>Agregar al carrito</button>
+                    </div>
+                </div>
+            </div>
+ 
+            <div style={{ height: 600 }}>
+                <div class="hero-image-dos">
+                    <img src="" alt=''/>
+                </div>
+            </div>
             
         </Fragment>
     );
