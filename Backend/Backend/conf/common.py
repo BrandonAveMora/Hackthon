4e130469a8271b302c71d0c8b96e66fbd36a7d11
Ray <rafaelgame2012@gmail.com>
2021-07-07 01:38:05 -0500

    common.py modified

--- Backend/Backend/conf/common.py
+++ Backend/Backend/conf/common.py
@@ -84,6 +84,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
